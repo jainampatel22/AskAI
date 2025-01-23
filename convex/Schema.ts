f6e5cbb9d7f2378c3cecfa9477f20a9d67cb2e09
=jainam patel <pateljainams122@gmail.com>
2025-01-23 19:44:51 +0530

    fixes

--- convex/Schema.ts
+++ convex/Schema.ts
@@ -1,17 +1,17 @@
-import {defineSchema,defineTable} from "convex/server"
-import {v} from "convex/values"
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
-    chats:defineTable({
-        title:v.string(),
-        userId:v.string(),
-        createdAt:v.number()
-    }).index('by_user',['userId']),
+  chats: defineTable({
+    title: v.string(),
+    userId: v.string(),
+    createdAt: v.number(),
+  }).index("by_user", ["userId"]),
 
-    messages:defineTable({
-        chatId:v.id('chats'),
-        content:v.string(),
-        role:v.union(v.literal('user'),v.literal('Agent')),
-        createdAt:v.number()
-    }).index('by_chat',['chatId'])
-})
+  messages: defineTable({
+    chatId: v.id("chats"),
+    content: v.string(),
+    role: v.union(v.literal("user"), v.literal("Agent")),
+    createdAt: v.number(),
+  }).index("by_chat", ["chatId"]),
+});
